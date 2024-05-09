2fa565c4da208b226210936c171724fa5f9553a7
EmiSan031 Sanchez <emisan031@gmail.com>
2024-05-09 06:23:21 +0000

    Se agrega notebook para trabajo Actividad Evaluable: Mapas de calor y boxplots

--- py.py
+++ py.py
@@ -1,7 +1,12 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from kneed import KneeLocator
 
-file_path = "./housing.csv"
-data = pd.read_csv(file_path)
+file_path = "clasesemanatec/housing.csv"
+df = pd.read_csv(file_path)
 
 print("Estadísticas descriptivas:")
-print(data.describe())
+print(df.describe())
